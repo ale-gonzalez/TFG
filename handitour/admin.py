b7816c9a5d589c9f49e503d17081f0e90b05caaf
Alejandro González Damin <44491214+agd@users.noreply.github.com>
2020-08-24 21:03:48 +0200

    modelo hecho y registado en admin

--- handitour/admin.py
+++ handitour/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Usuario, Ciudad, Valoracion, Monumento, Barrio, Aparcamiento, Transporte, Lineas, Parada
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Ciudad)
+admin.site.register(Valoracion)
+admin.site.register(Monumento)
+admin.site.register(Barrio)
+admin.site.register(Aparcamiento)
+admin.site.register(Transporte)
+admin.site.register(Lineas)
+admin.site.register(Parada)
--- handitour/models.py
+++ handitour/models.py
@@ -1,4 +1,99 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-# cambio
+
+class Usuario (models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    edad = models.IntegerField(max_length=2)
+    password = models.CharField(max_length=30)
+    foto = models.FileField(null=True, upload_to="img/usuario")
+
+    def __str__(self):
+        return self.nombre
+
+class Ciudad (models.Model):
+    nombre = models.CharField(max_length=30)
+    historia = models.CharField(max_length=500)
+    localizacion = models.CharField(max_length=20)
+    foto = models.FileField(null=True, upload_to="img/ciudad")
+    usuarios = models.ManyToManyField(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Monumento (models.Model):
+    nombre = models.CharField(max_length=30)
+    agno_inniaguracion = models.IntegerField (default=1, MaxValue=2020, MinValue=-10000)
+    historia = models.CharField(max_length=500)
+    localizacion = models.CharField(max_length=20)
+    precio_pers = models.PositiveIntegerField(default=0, max_length=2)
+    precio_disc = models.PositiveIntegerField(default=0, max_length=2)
+    foto = models.FileField(null=True, upload_to="img/ciudad")
+
+    def __str__(self):
+        return self.nombre
+
+class Valoracion (models.Model):
+    calificacion = models.PositiveIntegerField(default=1, MaxValue=100, MinValue=1)
+    comentario = models.CharField(max_length=500)
+    usuarios = models.ManyToManyField(Usuario, on_delete=models.CASCADE)
+    monumento = models.ForeignKey(Monumento, on_delete=models.CASCADE())
+
+    def __str__(self):
+        return self.comentario
+
+
+class Aparcamiento(models.Model):
+    localizacion = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.localizacion
+
+class Alojamiento (models.Model):
+    nombre = models.CharField(max_length=30)
+    calificacion = models.PositiveIntegerField(default=1, MaxValue=100, MinValue=1)
+    localizacion = models.CharField(max_length=20)
+    foto = models.FileField(null=True, upload_to="img/alojamiento")
+
+    def __str__(self):
+        return self.nombre
+
+class Lineas (models.Model):
+    origen = models.CharField(max_length=30)
+    destino = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.origen + " " + self.destino
+
+
+class Parada (models.Model):
+    nombre = models.CharField(max_length=30)
+    localizacion = models.CharField(max_length=30)
+    lineas = models.ManyToManyField(Lineas, on_delete=models.Model)
+
+    def __str__(self):
+        return self.nombre
+
+class Barrio (models.Model):
+    nombre = models.CharField(max_length=30)
+    historia = models.CharField(max_length=500)
+    localizacion = models.CharField(max_length=20)
+    foto = models.FileField(null=True, upload_to="img/barrio")
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    aparcamientos = models.ForeignKey(Aparcamiento, on_delete=models.CASCADE)
+    alojamientos = models.ForeignKey(Alojamiento, on_delete=models.CASCADE)
+    paradas = models.ForeignKey(Parada, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class Transporte (models.Model):
+    tipo = models.CharField(max_length=30)
+    usuarios = models.ManyToManyField(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tipo
+
