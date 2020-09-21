c219aac0c7b28778c448971dd2fc657c90c9f6c5
Alejandro González Damin <44491214+agd@users.noreply.github.com>
2020-09-21 13:18:07 +0200

    pruebas

--- handitour/static/js/ciudad.js
+++ handitour/static/js/ciudad.js
@@ -1,39 +1,26 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-let map;
-let service;
-let infowindow;
-
-function initMap() {
-  const sydney = new google.maps.LatLng(-33.867, 151.195);
-  infowindow = new google.maps.InfoWindow();
-  map = new google.maps.Map(document.getElementById("mapa"), {
-    center: sydney,
-    zoom: 15
-  });
-  const request = {
-    query: "Museum of Contemporary Art Australia",
-    fields: ["name", "geometry"]
-  };
-  service = new google.maps.places.PlacesService(map);
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (let i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-      map.setCenter(results[0].geometry.location);
+var geocoder;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
     }
-  });
-}
+    map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+  }
 
-function createMarker(place) {
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location
-  });
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name);
-    infowindow.open(map);
-  });
-}
+  function codeAddress() {
+    var address = document.getElementById('localizacion').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
