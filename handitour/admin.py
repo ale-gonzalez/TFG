ca7f3260a1bad3c90ba45d6eeace20cbf6a8d476
Alejandro González Damin <44491214+agd@users.noreply.github.com>
2020-10-25 13:40:51 +0100

    pruebas

--- handitour/admin.py
+++ handitour/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 from .models import Ciudad, Valoracion, Monumento, Barrio, Aparcamiento, Transporte, Linea, Parada
+from easy_maps.widgets import AddressWithMapWidget
+from .forms import ModelForm
 # Register your models here
-admin.site.register(Ciudad)
+
+
+class CiudadAdmin(admin.ModelAdmin):
+    class form(ModelForm):
+        class Meta:
+            widgets = {
+                'localizacion': AddressWithMapWidget({'class': 'vTextField'})
+            }
+
+class MunmemntoAdmin(admin.ModelAdmin):
+    class form(ModelForm):
+        class Meta:
+            widgets = {
+                'localizacion': AddressWithMapWidget({'class': 'vTextField'})
+            }
+
+
+
+admin.site.register(Ciudad,  CiudadAdmin)
 admin.site.register(Valoracion)
-admin.site.register(Monumento)
+admin.site.register(Monumento, MunmemntoAdmin)
 admin.site.register(Barrio)
 admin.site.register(Aparcamiento)
 admin.site.register(Transporte)
