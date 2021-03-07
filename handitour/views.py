720b9152b91bf3d7a39b347cc16d1db8cd750048
Alejandro González Damin <44491214+agd@users.noreply.github.com>
2021-03-07 19:07:26 +0100

    filtro

--- handitour/views.py
+++ handitour/views.py
@@ -78,8 +78,6 @@ def ciudad_alojamiento(request, id):
             seleccionado = form.cleaned_data.get('barrio')
             barrio = Barrio.objects.all().filter(nombre=seleccionado)
             return render(request, "alojamientos.html", {"alojamientos": alojamientos, "encabezado": ciudad.nombre.upper() + " - ALOJAMIENTOS", "ciudad": ciudad, "form": form, "barrio": barrio})
-        else:
-            return render(request, "alojamientos.html", {"alojamientos": alojamientos, "encabezado": ciudad.nombre.upper() + " - ALOJAMIENTOS", "ciudad": ciudad, "form": form})
     return render(request, "alojamientos.html", {"alojamientos": alojamientos, "encabezado": ciudad.nombre.upper() + " - ALOJAMIENTOS", "ciudad":  ciudad, "form": form})
 
 
