0bf8b7022e6cfda90e12596383542792888c9d18
Alejandro González Damin <44491214+agd@users.noreply.github.com>
2021-04-03 14:38:43 +0200

    js

---
+++ handitour/static/js/ciudad.js
@@ -0,0 +1,24 @@
+let map;
+function initMap(localizacionCiudad) {
+    $.ajax(
+        {
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + localizacionCiudad + '&key=AIzaSyDuqyO6rP6S_609rthhZt0dbi_uAHgpM8s',
+            type: 'GET',
+            dataType: 'text',
+            success: function (data) {
+                datos = JSON.parse(data);
+                map = new google.maps.Map(document.getElementById("mapa"), {
+                    center: {
+                        lat: parseFloat(datos.results[0].geometry.location.lat),
+                        lng: parseFloat(datos.results[0].geometry.location.lng)
+                    },
+                    zoom: 15,
+                });
+            },
+            error: function () {
+                alert("No hemos podido encontrar resultados");
+            },
+        });
+}
+
+
