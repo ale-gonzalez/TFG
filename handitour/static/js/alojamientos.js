db40b4be421952f96a38c5adfe66d0fc2a03f2c4
ale-gonzalez <agonzalez.alumno@iesrafaelalberti.es>
2021-04-05 21:10:29 +0200

    pue

---
+++ handitour/static/js/alojamientos.js
@@ -0,0 +1,59 @@
+let map;
+
+function initMap(localizacionCiudad){
+    $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + localizacionCiudad + '&key=AIzaSyDuqyO6rP6S_609rthhZt0dbi_uAHgpM8s',
+        type: 'GET',
+        dataType: 'text',
+        success: function (data) {
+            datos = JSON.parse(data);
+
+            map = new google.maps.Map(document.getElementById("mapa"), {
+                center: {lat: parseFloat(datos.results[0].geometry.location.lat) , lng: parseFloat(datos.results[0].geometry.location.lng)},
+                zoom: 15,
+            });
+        },
+        error: function () {
+            alert("No hemos podido encontrar resultados");
+        },
+    });
+}
+
+function getMarkers(localizacionAlojamiento){
+      $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+ localizacionAlojamiento+'&key=AIzaSyDuqyO6rP6S_609rthhZt0dbi_uAHgpM8s',
+        type: 'GET',
+        dataType: 'text',
+        success: function (data) {
+            datos = JSON.parse(data);
+            const marker = new google.maps.Marker({
+            position: {lat: parseFloat(datos.results[0].geometry.location.lat) , lng: parseFloat(datos.results[0].geometry.location.lng)},
+            map: map,
+          });
+        },
+        error: function () {
+            alert("No hemos podido encontrar resultados");
+        },
+    });
+
+}
+
+function getBarrio(localizacionBarrio){
+        $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+ localizacionBarrio +'&key=AIzaSyDuqyO6rP6S_609rthhZt0dbi_uAHgpM8s',
+        type: 'GET',
+        dataType: 'text',
+        success: function (data) {
+            datos = JSON.parse(data);
+
+            map = new google.maps.Map(document.getElementById("mapa"), {
+                center: {lat: parseFloat(datos.results[0].geometry.location.lat) , lng: parseFloat(datos.results[0].geometry.location.lng)},
+                types: ["lodging"],
+                zoom: 15,
+            });
+        },
+        error: function () {
+            alert("No hemos podido encontrar resultados");
+        },
+    });
+}
